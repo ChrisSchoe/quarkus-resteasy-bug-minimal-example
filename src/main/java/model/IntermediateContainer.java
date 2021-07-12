e5e4b1e4f10a6a73dc0349fd10d3296121f74885
Christoph Schoennenbeck <christoph.schoennenbeck@viadee.de>
2021-07-12 16:47:30 +0200

    add workaround
    Signed-off-by: Christoph Schoennenbeck <christoph.schoennenbeck@viadee.de>

--- src/main/java/model/IntermediateContainer.java
+++ src/main/java/model/IntermediateContainer.java
@@ -1,6 +1,15 @@
 package model;
 
 public abstract class IntermediateContainer<T extends AbstractChild> extends AbstractContainer<T> {
+  @Override
+  public T getChild() {
+    return this.child;
+  }
+
+  @Override
+  public void setChild(T child) {
+    this.child = child;
+  }
 
 
 }
